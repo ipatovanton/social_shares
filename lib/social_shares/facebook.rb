6d0f46a6aaffb7628579bd126fb2d3c8140f63c1
Anton <ipatovanton@gmail.com>
2017-03-24 15:05:11 +0300

    Update facebook.rb

--- lib/social_shares/facebook.rb
+++ lib/social_shares/facebook.rb
@@ -4,7 +4,7 @@ module SocialShares
 
     def shares!
       response = get(URL, params: {
-        access_token: <%= ENV["facebook_token"] %>,
+        access_token: ENV["facebook_token"],
         id: checked_url,
         fields: 'share'
       })
